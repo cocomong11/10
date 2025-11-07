96988ec848d14f46d922d2efc20f0db79859a6da
cocomong11 <carpediem1011@naver.com>
2025-11-07 19:18:46 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,17 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int i=0;
-	char str[4];
-	str[0]='a';
-	str[1]='b';
-	str[2]='c';
-	str[3]='\0';
+	char src[]="The worst things to eat before you sleep";
+	char dst[100];
+	strcpy(dst,src);
+	printf("copied string:%s\n",dst);
+	system("PAUSE");
+	return 0;
 	
-	while(str[i] != '\0'){
-		printf("%c",str[i]);
-		i++;
-		
-	}
 
 }
